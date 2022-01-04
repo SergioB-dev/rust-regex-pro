53ab03b4f78bc4954c754c1b17a034d1a229238f
pavlos-mac <pavlos.macdonald-kosmidis@uk.clara.net>
2022-01-04 15:29:59 +0000

    * prevent showing next question if given regex is wrong * Pass question index to show which question user is on

--- src/questions.rs
+++ src/questions.rs
@@ -22,8 +22,8 @@ pub struct Question {
 }
 
 impl Question {
-    pub fn ask_user_question(&self, user: &mut User) {
-        println!("As your first challenge, come up with a clever regex to capture this: \n\n\n\n");
+    pub fn ask_user_question(&self, user: &mut User, i: &i8) -> bool {
+        println!("For challenge number {}, come up with a clever regex to capture this: \n\n\n", i);
         let string_to_display = self.produce_user_facing_string();
         println!("{}", string_to_display);
         println!("[-] Extract --> {} <--", self.search_string);
@@ -36,12 +36,14 @@ impl Question {
             .unwrap_or(false);
 
         if regex_is_correct {
-            println!("Correct");
+            println!("\n\nCorrect regex, well done :)\n\n");
             user.correct += 1;
             user.score += self.points;
+            return true;
         } else {
-            println!("Wrong");
+            println!("\n\nIncorrect regex, try again :/\n\n");
             user.wrong += 1;
+            return false;
         }
     }
 
